9b46c91a4743f55c6b82c57c22a39fb4bb5a2256
Kieran Northime <kieran@blackeyetech.com>
2021-07-24 09:27:57 +0100

    updated

--- src/test-s3.ts
+++ src/test-s3.ts
@@ -1,5 +1,7 @@
 import CNShell from "cn-shell";
 import * as AWS from "./main";
+// import request from "request";
+// import fs from "fs";
 
 class App extends CNShell {
   constructor(name: string) {
@@ -16,7 +18,7 @@ class App extends CNShell {
     let bucket = process.env["TEST_BUCKET"];
 
     if (bucket === undefined) {
-      throw new Error("Must set env var TEST_BUCEKT");
+      throw new Error("Must set env var TEST_BUCKET");
     }
 
     let test = new AWS.S3.Bucket("Bucket", {
@@ -29,13 +31,32 @@ class App extends CNShell {
     this.info("Files: %j", files);
 
     let url = await test.presignFileRequest(
-      "getObject",
-      "docs/water/site/1",
-      "Summary Report.pdf",
+      "putObject",
+      "docs/water",
+      "test.pdf",
       300,
+      "application/pdf",
     );
 
     this.info("url: %s", url);
+
+    // request(
+    //   {
+    //     method: "PUT",
+    //     uri: <string>url,
+    //     body: fs.readFileSync("./src/test.pdf"),
+    //     headers: {
+    //       "Content-Type": "application/pdf",I'm out
+    //     },
+    //   },
+    //   (err: any, res) => {
+    //     if (err) {
+    //       this.error(err);
+    //     } else {
+    //       this.info("upload successful: %j", res.statusCode);
+    //     }
+    //   },
+    // );
 
     let success = await test.deleteFile("docs/water/site/1/Summary Report.pdf");
     this.info("Deleted succes? %j", success);
